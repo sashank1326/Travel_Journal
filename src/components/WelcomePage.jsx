0fe3d6a611248bcbf1e216926307ebbd7725dd4e
sashank1326 <sashankemmidi1326@gmail.com>
2025-02-03 22:54:53 +0530

    Welcome page design

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,61 +1,37 @@
-// import React from "react";
-
-// const WelcomePage = () => {
-//   return (
-//     <div 
-//       className="h-screen w-full flex flex-col items-center justify-center bg-cover bg-center relative"
-//       //style={{ backgroundImage: "url('../../public/images/Background.jpeg')" }}
-//       style={{ backgroundImage: `url('../../public/images/project3.jpeg')` }}
-//     >
-//       <div className="absolute top-0 left-0 w-full h-full "></div>
-      
-//       <div className="relative z-10 flex flex-col items-center">
-//         <img 
-//           src="../../public/images/logo.jpg" 
-//           alt="Logo" 
-//           className="w-32 h-32 object-contain mb-4"
-//         />
-//         <h1 className="text-white text-4xl font-bold">Travel Journal</h1>
-//         <p className="text-white text-lg">Travel To Live, Live To Travel</p>
-//       </div>
-      
-//       <div className="relative z-10 mt-10 flex gap-4">
-//         <button className="px-6 py-2 bg-white text-black rounded-full text-lg">Sign In</button>
-//         <button className="px-6 py-2 bg-white text-black rounded-full text-lg">Sign Up</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default WelcomePage;
-
-
-
 import React from "react";
 import image from '../../public/images/logo.jpg'
 
 const WelcomePage = () => {
   return (
+    <div>
     <div 
-      className="h-screen w-full flex flex-col items-center bg-cover bg-center relative"
-      style={{ backgroundImage: "url('../../public/images/background.jpeg')" }}
+      className="h-screen w-full flex flex-col  bg-cover bg-center relative gap-0 "
+      style={{ backgroundImage: "url('../../public/images/bg1.jpg')" }}
     >
-      <div className="absolute top-10 left-0 w-full h-full bg-opacity-50"></div>
-      
-      <div className="relative z-10 mt-15 flex flex-col items-center">
-        <img 
+     
+      <div className="flex flex-col items-center bg-cover bg-center ">
+      <div className="relative z-10 flex mt-45 flex-row items-center gap-8">
+      <img 
           src={image} 
-          alt="Logo"  
-          className="w-32 h-32 object-contain mb-4"
+          alt="Logo"
+          className="w-32 h-32 object-contain mb-4 rounded-md"
         />
-        <h1 className="text-white text-4xl font-bold">Travel Journal</h1>
-        <p className="text-black text-lg">Travel To Live, Live To Travel</p>
+        <div className="flex items-center flex-col">
+        <h1 className="text-black text-5xl font-bold">Travel Journal</h1>
+        <p className="text-white text-lg">Travel To Live, Live To Travel</p>
+        </div>
+        
       </div>
       
-      <div className="relative z-10 mt-10 flex gap-10">
+      <div className="relative z-10 mt-10 flex gap-5">
         <button className="px-6 py-2 bg-white text-black rounded-full text-lg">Sign In</button>
         <button className="px-6 py-2 bg-white text-black rounded-full text-lg">Sign Up</button>
+
       </div>
+      
+    </div>
+    <p className="bottom-20 text-white text-4xl  fixed left-0 right-0 text-center">Description</p>
+    </div> 
     </div>
   );
 };
